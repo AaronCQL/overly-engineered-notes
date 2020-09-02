c53a5cf223a5b49a0de5dcc31332b4fbb236f1df
Aaron Choo <choozy.aaron@gmail.com>
2020-09-02 21:32:29 +0800

    fix: add env port

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,10 @@ import express, { Express } from "express";
 import notesRouter from "./routes/notes";
 
 const app: Express = express();
+const port: string = process.env.PORT || "8000";
 
 app.use("/notes", notesRouter);
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("Server started at Port 8000");
 });
