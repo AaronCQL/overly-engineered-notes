88a45fbd8c2de3e37693b286a2d470fe79ca943f
Aaron Choo <choozy.aaron@gmail.com>
2020-09-08 11:37:12 +0800

    feat: append /api to all API endpoints

--- server/src/services/serverService.ts
+++ server/src/services/serverService.ts
@@ -1,14 +1,17 @@
-import express, { Express } from "express";
+import express, { Express, Router } from "express";
 
 import notesRouter from "./../routes/notes";
 
 const app: Express = express();
+const router: Router = express.Router();
 
 // default express body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// append `/api` prefix to all routes
+app.use("/api", router);
 
 // routes
-app.use("/notes", notesRouter);
+router.use("/notes", notesRouter);
 
 export default app;
--- server/tests/routes/notes.test.ts
+++ server/tests/routes/notes.test.ts
@@ -2,6 +2,8 @@ import request from "supertest";
 import server from "../../src/services/serverService";
 import { initDb, closeDb } from "../../src/services/dbService";
 
+const API_ENDPOINT = "/api/notes";
+
 beforeAll(async (done) => {
   // initialise the testing DB before starting
   await initDb();
@@ -16,7 +18,7 @@ afterAll(async (done) => {
 
 describe("GET request", () => {
   it("should return 200 and an array of notes", async (done) => {
-    const res = await request(server).get("/notes").send();
+    const res = await request(server).get(API_ENDPOINT).send();
 
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.status).toEqual(200);
@@ -27,7 +29,7 @@ describe("GET request", () => {
 
 describe("POST request", () => {
   it("should return 400 if 'text' parameter is missing", async (done) => {
-    const res = await request(server).post("/notes").send();
+    const res = await request(server).post(API_ENDPOINT).send();
 
     expect(res.status).toEqual(400);
     done();
@@ -35,7 +37,7 @@ describe("POST request", () => {
 
   it("should return 400 if 'text' paramter is invalid", async (done) => {
     const invalidData = { text: " " };
-    const res = await request(server).post("/notes").send(invalidData);
+    const res = await request(server).post(API_ENDPOINT).send(invalidData);
 
     expect(res.status).toEqual(400);
     done();
@@ -43,7 +45,7 @@ describe("POST request", () => {
 
   it("should return 201 and the inserted data", async (done) => {
     const validData = { text: "hello!" };
-    const res = await request(server).post("/notes").send(validData);
+    const res = await request(server).post(API_ENDPOINT).send(validData);
 
     expect(res.status).toEqual(201);
     expect(res.body.text).toEqual(validData.text);
@@ -56,7 +58,9 @@ describe("PUT request", () => {
   const invalidObjectId = "123";
 
   it("should return 400 if 'text' parameter is missing", async (done) => {
-    const res = await request(server).put(`/notes/${invalidObjectId}`).send();
+    const res = await request(server)
+      .put(`${API_ENDPOINT}/${invalidObjectId}`)
+      .send();
 
     expect(res.status).toEqual(400);
     done();
@@ -65,7 +69,7 @@ describe("PUT request", () => {
   it("should return 400 if 'text' parameter is invalid", async (done) => {
     const invalidData = { text: " " };
     const res = await request(server)
-      .put(`/notes/${invalidObjectId}`)
+      .put(`${API_ENDPOINT}/${invalidObjectId}`)
       .send(invalidData);
 
     expect(res.status).toEqual(400);
@@ -75,7 +79,7 @@ describe("PUT request", () => {
   it("should return 404 if document is not found", async (done) => {
     const validData = { text: "hello" };
     const res = await request(server)
-      .put(`/notes/${invalidObjectId}`)
+      .put(`${API_ENDPOINT}/${invalidObjectId}`)
       .send(validData);
 
     expect(res.status).toEqual(404);
@@ -89,7 +93,7 @@ describe("DELETE request", () => {
 
   it("should return 404 if document is not found", async (done) => {
     const res = await request(server)
-      .delete(`/notes/${invalidObjectId}`)
+      .delete(`${API_ENDPOINT}/${invalidObjectId}`)
       .send();
 
     expect(res.status).toEqual(404);
