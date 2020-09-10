e014780288cc57058f598d3243b1beea54a62ccb
Aaron Choo <choozy.aaron@gmail.com>
2020-09-10 16:41:25 +0800

    feat: init client (vite, tailwind, prettier, eslint)

---
+++ client/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+    "prettier/vue",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-5xl">App.vue</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
