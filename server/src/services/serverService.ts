d685f69e7e00016db206f716b66167f6b36f3a0f
Aaron Choo <choozy.aaron@gmail.com>
2020-09-14 12:51:10 +0800

    feat: enable cors

--- server/src/services/serverService.ts
+++ server/src/services/serverService.ts
@@ -1,10 +1,12 @@
 import express, { Express, Router } from "express";
+import cors from "cors";
 
 import notesRouter from "./../routes/notes";
 
 const app: Express = express();
 const router: Router = express.Router();
 
+app.use(cors());
 // default express body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
